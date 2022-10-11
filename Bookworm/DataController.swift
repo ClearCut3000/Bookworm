e60b9da128ecb2c144edda35fcb5d51fcab18774
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-10-11 22:42:38 +0300

    Initial commit

---
+++ Bookworm/DataController.swift
@@ -0,0 +1,13 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Николай Никитин on 11.10.2022.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+
+}
